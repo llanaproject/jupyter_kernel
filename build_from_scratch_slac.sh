78dae18f1afdecb90e887dd2f8f7aca7f23a5d6d
Muammar El Khatib <melkhatibr@lbl.gov>
2020-09-15 08:16:45 -0700

    Update build_from_scratch_slac.sh
    Add ipywidgets.

--- build_from_scratch_slac.sh
+++ build_from_scratch_slac.sh
@@ -51,7 +51,7 @@ pip install dask distributed sklearn multipledispatch numba
 conda install -y -c conda-forge 'nodejs>=10'
 conda install -y ipykernel jupyterlab
 conda install -y -c plotly plotly
-conda install -y -c conda-forge matplotlib ipympl
+conda install -y -c conda-forge matplotlib ipympl ipywidgets
 conda install -y -c pyviz holoviews bokeh
 jupyter labextension install @pyviz/jupyterlab_pyviz
 
